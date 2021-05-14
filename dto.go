f2ad7dbab259197bbc800a4894db16d7d9caec54
sandeep.misal <sandeep.misal@deskera.org>
2021-05-14 17:55:14 +0530

    Added Get Document API

--- dto.go
+++ dto.go
@@ -278,3 +278,10 @@ type TransferResponse struct {
 	Metadata      interface{} `json:"metadata"`
 	CorrelationID string      `json:"correlationId"`
 }
+
+type Document struct {
+	ID      string `json:"id"`
+	Status  string `json:"status"`
+	Type    string `json:"type"`
+	Created string `json:"created"`
+}
